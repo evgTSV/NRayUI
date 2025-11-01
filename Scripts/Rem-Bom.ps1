5f4dc95e655a2608c75c92abf8379acdc178bfb7
evgTSV <freefiery@gmail.com>
2025-11-02 01:06:37 +0500

    Enhance Rem-Bom.ps1 script

--- Scripts/Rem-Bom.ps1
+++ Scripts/Rem-Bom.ps1
@@ -1,6 +1,27 @@
 Write-Host "Removing BOM from files..." -ForegroundColor Yellow
 
-Get-ChildItem -Recurse -Include *.fs,*.fsx,.editorconfig,*.json,*.ps1,*.fsproj,*.fantomasignore,*.sln,*.md | ForEach-Object {
-    $content = [System.IO.File]::ReadAllText($_.FullName, [System.Text.Encoding]::UTF8)
-    [System.IO.File]::WriteAllText($_.FullName, $content, [System.Text.UTF8Encoding]::new($false))
+# https://stackoverflow.com/questions/6119956/how-to-determine-if-git-handles-a-file-as-binary-or-as-text#comment15281840_6134127
+$nullHash = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+$textFiles = git -c core.quotepath=off diff --numstat $nullHash HEAD -- @allFiles |
+    Where-Object { -not $_.StartsWith('-') } |
+    ForEach-Object { [Regex]::Unescape($_.Split("`t", 3)[2]) }
+Write-Output "Text files in the repository: $($textFiles.Length)"
+
+[array] $excludeExtensions = @('.dotsettings')
+
+try
+{
+    foreach ($file in $textFiles)
+    {
+        if ($excludeExtensions -contains [IO.Path]::GetExtension($file).ToLowerInvariant())
+        {
+            continue
+        }
+
+        $fullPath = Resolve-Path -LiteralPath $file
+        $content = [System.IO.File]::ReadAllText($fullPath, [System.Text.Encoding]::UTF8)
+        [System.IO.File]::WriteAllText($fullPath, $content, [System.Text.UTF8Encoding]::new($false))
+    }
+} finally {
+    Pop-Location
 }
